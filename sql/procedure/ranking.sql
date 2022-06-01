da4e0cfea31db76566cc69ea9166bdc463fa32bc
Yuri JI <dbfl9711@gmail.com>
2022-06-01 14:50:03 +0900

    add: 과목 별 최대 정원

--- sql/procedure/ranking.sql
+++ sql/procedure/ranking.sql
@@ -3,9 +3,9 @@ CREATE OR REPLACE PROCEDURE RANKING
     TYPE s_id_table IS TABLE OF enroll.s_id%TYPE INDEX BY BINARY_INTEGER;
     TYPE c_id_table IS TABLE OF enroll.c_id%TYPE INDEX BY BINARY_INTEGER;
 
-    CURSOR final_student IS ( SELECT c_name, c_id, c_no, e.s_id, s_name, RANK() OVER (PARTITION BY c_id, c_no ORDER BY s.s_grade desc, s.last_credit desc, s.last_score desc) RANK
-                              FROM ENROLL e , STUDENTS s 
-                              WHERE e.s_id = s.s_id
+    CURSOR final_student IS ( SELECT e.c_name, e.c_id, e.c_no, e.s_id, s_name, RANK() OVER (PARTITION BY e.c_id, e.c_no ORDER BY s.s_grade desc, s.last_credit desc, s.last_score desc) RANK, c_max
+                              FROM ENROLL e , STUDENTS s, COURSE c
+                              WHERE e.s_id = s.s_id and (e.c_id = c.c_id and e.c_no = c.c_no)
                               ORDER BY c_name, rank;
     )
 IS
