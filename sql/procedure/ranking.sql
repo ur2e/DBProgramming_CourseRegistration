2c04f74285a7807c676abb9843e5ac7374decba9
Yuri JI <dbfl9711@gmail.com>
2022-06-03 17:39:25 +0900

    update: e_state 추가

--- sql/procedure/ranking.sql
+++ sql/procedure/ranking.sql
@@ -1,18 +1,15 @@
 CREATE OR REPLACE PROCEDURE RANKING
-(
-    CURSOR final_student IS 
-    (   
+IS
+    CURSOR final_student IS (
         SELECT *
         FROM (
-            SELECT e.c_name, e.c_id, e.c_no, e.s_id, s_name, DENSE_RANK() OVER (PARTITION BY e.c_id, e.c_no ORDER BY s.s_grade desc, s.last_credit desc, s.last_score desc) RANK, c_max
+            SELECT e.c_name, e.c_id, e.c_no, e.s_id, s_name, DENSE_RANK() OVER (PARTITION BY e.c_id, e.c_no ORDER BY s.s_grade desc, s.last_credit desc, s.last_score desc) RANK, c_max, e_state
             FROM ENROLL e , STUDENTS s, COURSE c
             WHERE e.s_id = s.s_id and (e.c_id = c.c_id and e.c_no = c.c_no)
-            ORDER BY c_name, rank
+            ORDER BY c_name, rank;
         )
-        WHERE RANK <= c_max;
-    )
-IS
-
+        WHERE RANK <= 1
+    );
 BEGIN
     OPEN final_student;    
     LOOP 
