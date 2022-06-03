5ce7d95ef34c3eba6e1ea3aa9a758e619c4c2a3c
juyonLee00 <ljy012900@gmail.com>
2022-06-03 17:27:50 +0900

    ranking.sql 수정

--- sql/procedure/ranking.sql
+++ sql/procedure/ranking.sql
@@ -1,7 +1,6 @@
 CREATE OR REPLACE PROCEDURE RANKING
-(
+IS
     CURSOR final_student IS 
-    (   
         SELECT *
         FROM (
             SELECT e.c_name, e.c_id, e.c_no, e.s_id, s_name, DENSE_RANK() OVER (PARTITION BY e.c_id, e.c_no ORDER BY s.s_grade desc, s.last_credit desc, s.last_score desc) RANK, c_max
@@ -9,15 +8,13 @@ CREATE OR REPLACE PROCEDURE RANKING
             WHERE e.s_id = s.s_id and (e.c_id = c.c_id and e.c_no = c.c_no)
             ORDER BY c_name, rank
         )
-        WHERE RANK <= c_max;
-    )
-IS
+        WHERE RANK <= 1;
 
 BEGIN
     OPEN final_student;    
     LOOP 
         UPDATE ENROLL
-        SET e_state = 'í™•ì •';
+        SET e_state = 'È®Á¤';
         EXIT WHEN final_student%NOTFOUND;
     END LOOP;
 
