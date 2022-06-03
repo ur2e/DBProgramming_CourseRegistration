d208407d7679533455e75ccaf580a247bb3b1a6c
Yuri JI <dbfl9711@gmail.com>
2022-06-03 16:08:31 +0900

    add: ranking.sql

--- sql/procedure/ranking.sql
+++ sql/procedure/ranking.sql
@@ -3,21 +3,25 @@ CREATE OR REPLACE PROCEDURE RANKING
     TYPE s_id_table IS TABLE OF enroll.s_id%TYPE INDEX BY BINARY_INTEGER;
     TYPE c_id_table IS TABLE OF enroll.c_id%TYPE INDEX BY BINARY_INTEGER;
 
-    CURSOR final_student IS ( SELECT e.c_name, e.c_id, e.c_no, e.s_id, s_name, RANK() OVER (PARTITION BY e.c_id, e.c_no ORDER BY s.s_grade desc, s.last_credit desc, s.last_score desc) RANK, c_max
-                              FROM ENROLL e , STUDENTS s, COURSE c
-                              WHERE e.s_id = s.s_id and (e.c_id = c.c_id and e.c_no = c.c_no)
-                              ORDER BY c_name, rank;
+    CURSOR final_student IS 
+    (   
+        SELECT *
+        FROM (
+            SELECT e.c_name, e.c_id, e.c_no, e.s_id, s_name, DENSE_RANK() OVER (PARTITION BY e.c_id, e.c_no ORDER BY s.s_grade desc, s.last_credit desc, s.last_score desc) RANK, c_max
+            FROM ENROLL e , STUDENTS s, COURSE c
+            WHERE e.s_id = s.s_id and (e.c_id = c.c_id and e.c_no = c.c_no)
+            ORDER BY c_name, rank
+        )
+        WHERE RANK <= c_max;
     )
 IS
-    -- 각 과목별 max 정원
-    v_c_max  NUMBER(3);
+
 BEGIN
-    OPEN final_student;
-        
+    OPEN final_student;    
     LOOP 
 
-        FETCH final_student INTO ;
-
+        UPDATE ENROLL
+        SET e_state = '확정';
 
         EXIT WHEN final_student%NOTFOUND;
     END LOOP;
