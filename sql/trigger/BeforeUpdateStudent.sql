55377ae864624e2218072ac1bd480629e34087f1
Yuri JI <dbfl9711@gmail.com>
2022-06-05 23:20:49 +0900

    add: 비밀번호 NULL 값 오류

--- sql/trigger/BeforeUpdateStudent.sql
+++ sql/trigger/BeforeUpdateStudent.sql
@@ -4,22 +4,26 @@ FOR EACH ROW
 DECLARE
    underflow_length EXCEPTION;
    invalid_value EXCEPTION;
-   nBlank NUMBER;    --���� ����
+   null_value EXCEPTION;
+   nBlank NUMBER;    --공백 개수
 
 BEGIN
-/* 4�ڸ� ���� */
+/* 4자리 이하 */
 IF (LENGTH(:new.s_pwd) < 4) THEN
    RAISE underflow_length;
 END IF;
 
-/* ���� ���� */
+/* 공란 포함 */
 IF (:new.s_pwd LIKE '% %') THEN 
    RAISE invalid_value;
 END IF;
 
+/* NULL 값 처리*/
+IF( :new.s_pwd is null ) THEN 
+   RAISE null_value;
+END IF ;
 
-
-/* ���� ���� */
+/* 정상 동작 */
 UPDATE students s 
 SET s.s_pwd = :new.s_pwd
 WHERE s.s_id = :new.s_id;
@@ -27,12 +31,15 @@ WHERE s.s_id = :new.s_id;
 
 EXCEPTION
 WHEN underflow_length THEN
-   RAISE_APPLICATION_ERROR(-20002, '��ȣ�� 4�ڸ� �̻��̾�� �մϴ�');
-   -- ����â ����, ���� ���� ���
+   RAISE_APPLICATION_ERROR(-20002, '암호는 4자리 이상이어야 합니다.');
+
 WHEN invalid_value THEN 
-   RAISE_APPLICATION_ERROR(-20003, '��ȣ�� ������ �Էµ��� �ʽ��ϴ�');
--- ����â ����, ���� ���� ���
+   RAISE_APPLICATION_ERROR(-20003, '암호는 공란은 입력되지 않습니다.');
+
+WHEN null_value THEN 
+   RAISE_APPLICATION_ERROR(-20004, '암호를 입력한 후에 다시 시도해주세요.');
+
 WHEN OTHERS THEN
-   DBMS_OUTPUT.PUT_LINE('���� �߻�');
+   DBMS_OUTPUT.PUT_LINE('오류 발생');
 END;
 /
--- src/main/webapp/update_verify.jsp
+++ src/main/webapp/update_verify.jsp
@@ -34,6 +34,7 @@
       System.out.println(ex.getErrorCode());
       if (ex.getErrorCode() == 20002) sMessage="암호는 4자리 이상이어야 합니다";
       else if (ex.getErrorCode() == 20003) sMessage="암호에 공란은 입력되지 않습니다.";
+      else if (ex.getErrorCode() == 20004) sMessage="암호를 입력한 후에 다시 시도해주세요.";
       else sMessage="잠시 후 다시 시도하십시오"; 
    %>
    <script>
